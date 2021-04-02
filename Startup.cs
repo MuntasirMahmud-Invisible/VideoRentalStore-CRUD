9bd0a80066b634e0997e81c39089d684f2d26fd8
Muntasir Mahmud <muntasirmahmuddhruba@gmail.com>
2021-04-02 22:23:08 +0600

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoRentalApps.Startup))]
+namespace VideoRentalApps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
