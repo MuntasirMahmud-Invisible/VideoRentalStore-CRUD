b7bf85c9363fa6e72707a20a7d170fab128f9f3b
Muntasir Mahmud <muntasirmahmuddhruba@gmail.com>
2021-05-11 22:41:35 +0600

    Build  API for Customers and movies.

---
+++ Controllers/Api/CustomersController.cs
@@ -0,0 +1,102 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VideoRentalApps.Dtos;
+using VideoRentalApps.Models;
+
+namespace VideoRentalApps.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //GET: Api/Customers
+        public IHttpActionResult GetCustomers()
+        {
+            var customerDtos = _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+
+            return Ok(customerDtos);
+        }
+
+        //GET: Api/Customers/1
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            
+            if (customer == null)
+            {
+                return NotFound();
+            }
+                
+            return Ok(Mapper.Map<Customer, CustomerDto>(customer));
+        }
+
+        //POST: Api/Customers
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            customerDto.Id = customer.Id;
+
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
+        }
+
+        //PUT: Api/Customers/1
+        [HttpPut]
+        public IHttpActionResult UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+            Mapper.Map(customerDto, customerInDb);
+
+            /*If automapper doesn't use*/
+
+            /* customerInDb.Name = customer.Name;
+             customerInDb.DateofBirth = customer.DateofBirth;
+             customerInDb.MembershipTypeId = customer.MembershipTypeId;
+             customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;*/
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        //Delete: Api/Customers/1
+        [HttpDelete]
+        public IHttpActionResult RemoveCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ Dtos/CustomerDto.cs
@@ -0,0 +1,20 @@
+﻿    using System;
+using System.ComponentModel.DataAnnotations;
+using VideoRentalApps.Models;
+
+namespace VideoRentalApps.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [StringLength(255)]
+        public string Name { get; set; }
+
+     //   [Min18YearsIfaMember]
+        public DateTime? DateofBirth { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public byte MembershipTypeId { get; set; }
+    }
+}
