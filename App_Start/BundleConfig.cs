54cacdab866f23fec7a4eb4c8a588b2e413e6929
Muntasir Mahmud <muntasirmahmuddhruba@gmail.com>
2021-07-01 21:34:36 +0600

    Add Toastr to give message when submit is called

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -15,7 +15,8 @@ namespace VideoRentalApps
                         "~/Scripts/respond.js",
                         "~/scripts/datatables/jquery.datatables.js",
                         "~/scripts/datatables/datatables.bootstrap.js",
-                        "~/scripts/typeahead.bundle.js"
+                        "~/scripts/typeahead.bundle.js",
+                        "~/scripts/toastr.js"
                       ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -30,6 +31,7 @@ namespace VideoRentalApps
                       "~/Content/bootstrap-Lumen.css",
                       "~/content/datatables/css/datatables.bootstrap.css",
                       "~/Content/typeahead.css",
+                      "~/Content/toastr.css",
                       "~/Content/site.css"));
         }
     }
