0ab517b23b6ede786ab9384596287dfad05e4f50
Muntasir Mahmud <muntasirmahmuddhruba@gmail.com>
2021-07-23 22:12:43 +0600

    Unwanted Changes

---
+++ Migrations/202107231610375_SomeUnWantedChanges.cs
@@ -0,0 +1,26 @@
+﻿namespace VideoRentalApps.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SomeUnWantedChanges : DbMigration
+    {
+        public override void Up()
+        {
+            DropTable("dbo.RoleNames");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.RoleNames",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+    }
+}
