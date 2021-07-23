49e03b2cd830261bd3c5aa40ed5421fa6186fbb4
Muntasir Mahmud <muntasirmahmuddhruba@gmail.com>
2021-07-23 22:28:31 +0600

    Separate Application User/DbContext classes

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace VideoRentalApps.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MembershipType> MemberShipTypes { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<NewRental> Rentals { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
