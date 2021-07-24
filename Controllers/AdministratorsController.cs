1243b35a39d862287c1285bfac9172fb3ff468a9
Muntasir Mahmud <muntasirmahmuddhruba@gmail.com>
2021-07-25 01:45:19 +0600

    Add Administrator

---
+++ Controllers/AdministratorsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using VideoRentalApps.Models;
+using VideoRentalApps.ViewModels;
+
+namespace VideoRentalApps.Controllers
+{
+    public class AdministratorsController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministratorsController()
+        {
+
+        }
+        public AdministratorsController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+
+        }
+
+        //  GET: Administrator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult NewRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> NewRole(NewRoleViewModel model)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName,
+                };
+                var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                var roleManager = new RoleManager<IdentityRole>(roleStore);
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error);
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ ViewModels/NewRoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoRentalApps.ViewModels
+{
+    public class NewRoleViewModel
+    {
+
+        [Required]
+        [Display(Name = "User Role Name")]
+        public string RoleName { get; set; }
+
+
+    }
+}
